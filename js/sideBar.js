845e84dc96150f7f9450be79c15d708d39d9dce4
Murilo Gotardo <murilogotardo08@gmail.com>
2022-06-19 14:44:57 -0300

    initial commit

---
+++ js/sideBar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("open").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("open").style.display = "inline-block"
+}
